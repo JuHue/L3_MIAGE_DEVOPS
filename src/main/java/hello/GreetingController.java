56f1ef1fe7189f9c9d3e265b6aecdd2ee1e95f2f
JuHue <julienhue94@gmail.com>
2022-05-13 13:13:24 +0200

    new ver

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,39 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    @RequestMapping("/movies")
+    public String movies(){
+        List<String> listMovies = new ArrayList<String>();
+        String sqlSelectAllMovies = "SELECT * FROM movies";
+        String connectionUrl = "jdbc:mysql://127.0.0.1:3306/movies?serverTimezone=UTC";
+
+        try (Connection conn = DriveManager.getConnection(connectionUrl, "root", "admin"); 
+            PreparedStatement ps = conn.PreparedStatement(sqlSelectAllMovies);
+            ResultatSet rs = ps.executeQuery()) {
+
+                while (rs.next()) {
+                    System.out.print("ICI");
+                    String title = rs.getString("title");
+                    listMovies.add(title);
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            return listMovies.toString();
+        }
+    }
